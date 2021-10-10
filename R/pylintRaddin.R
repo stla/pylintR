ea5a507e4ba48d02d3270b65124e1748be57aa5f
stla <laurent_step@yahoo.fr>
2021-10-10 19:26:06 +0200

    addins

--- R/pylintRaddin.R
+++ R/pylintRaddin.R
@@ -6,11 +6,35 @@ pylintFile <- function(){
   }else{
     stop("This version of RStudio is too old!")
   }
-  contents <- paste0(editorContext[["contents"]], collapse = "\n")
-
+  if(editorContext[["path"]] != ""){
+    currentFolder <- dirname(editorContext[["path"]])
+    wd <- setwd(currentFolder)
+    on.exit(setwd(wd))
+    currentFile <- basename(editorContext[["path"]])
+  }else{
+    tmpdir <- tempdir()
+    untitled <- file.path(tmpdir, "untitled")
+    writeLines(editorContext[["contents"]], untitled)
+    wd <- setwd(tmpdir)
+    on.exit(setwd(wd))
+    currentFile <- "untitled"
+  }
+  pylint(currentFile)
 }
 
 #' @keywords internal
 pylintFolder <- function(){
-
+  if(hasFun("getSourceEditorContext")){
+    editorContext <- getSourceEditorContext()
+  }else{
+    stop("This version of RStudio is too old!")
+  }
+  if(editorContext[["path"]] != ""){
+    currentFolder <- dirname(editorContext[["path"]])
+    wd <- setwd(currentFolder)
+    on.exit(setwd(wd))
+  }else{
+    currentFolder <- getwd()
+  }
+  pylint(currentFolder)
 }
